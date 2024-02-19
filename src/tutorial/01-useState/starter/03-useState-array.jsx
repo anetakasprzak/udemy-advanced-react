d55d45fe304a252459510dea7297e8f50f8fe548
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-02-19 12:31:15 +0000

    setting up the data

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+import { data } from "../../../data";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default UseStateArray;
