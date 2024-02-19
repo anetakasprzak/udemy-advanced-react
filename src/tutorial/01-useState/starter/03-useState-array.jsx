820bb55b13f020db9abe51904d7ab18f4c380403
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-02-19 12:49:30 +0000

    removing item from an array

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -4,6 +4,15 @@ import { data } from "../../../data";
 const UseStateArray = () => {
   const [people, setPeople] = useState(data);
 
+  const removeItem = (id) => {
+    console.log(id);
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+  const clearAllItems = () => {
+    setPeople([]);
+  };
+
   return (
     <div>
       {people.map((person) => {
@@ -11,9 +20,20 @@ const UseStateArray = () => {
         return (
           <div key={id}>
             <h4>{name}</h4>
+            <button type="button" onClick={() => removeItem(id)}>
+              remove
+            </button>
           </div>
         );
       })}
+      <button
+        type="button"
+        style={{ marginTop: "2rem" }}
+        className="btn"
+        onClick={clearAllItems}
+      >
+        Clear items
+      </button>
     </div>
   );
 };
